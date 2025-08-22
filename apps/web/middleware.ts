2f11e8bd7f14d7ba67ba901db0c2c7ba369bc041
Joe Clark <jxclark@live.com>
2025-08-22 14:09:27 -0400

    04: added-clerk-orginization

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,10 +1,29 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublic = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+const isOrgFreeRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/org-selection(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublic(req)) {
+  const { userId, orgId } = await auth();
+
+  if (!isPublicRoute(req)) {
     await auth.protect();
+  }
+
+  if (userId && !orgId && !isOrgFreeRoute(req)) {
+    const searchParams = new URLSearchParams({ redirectIrl: req.url });
+
+    const orgSelection = new URL(
+      `/org-selection?${searchParams.toString()}`,
+      req.url
+    );
+    return NextResponse.redirect(orgSelection);
   }
 });
 
